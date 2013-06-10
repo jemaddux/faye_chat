cb5e910b0df78b3a17f35417e72c0defc904a987
John Maddux <jemaddux@gmail.com>
2013-06-10 15:49:04 -0600

    FIRST

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayeChat::Application.config.secret_token = 'f72ea444f9eed364f22293fc3de3bb5dbf8832283cdb53e612ba6c007e38a44a92c318a7bd2bbfe6b77e1d90a0da61991a12d1701fbaaf373f5927a391cb92ac'
